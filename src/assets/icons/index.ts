b7716181891c179957cfdcdfd85f74066f75fce4
M8Cpr <matteo.caprotti8@gmail.com>
2026-03-16 13:21:19 +0100

    feat: added mh icons for attack types and elements

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,13 @@
+import type { FC, SVGProps } from "react";
+
+export type SvgIcon = FC<SVGProps<SVGSVGElement>>;
+
+export { default as ElementDragon } from "./element-dragon.svg?react";
+export { default as ElementFire } from "./element-fire.svg?react";
+export { default as ElementIce } from "./element-ice.svg?react";
+export { default as ElementNone } from "./element-none.svg?react";
+export { default as ElementThunder } from "./element-thunder.svg?react";
+export { default as ElementWater } from "./element-water.svg?react";
+export { default as TypePower } from "./type-power.svg?react";
+export { default as TypeSpeed } from "./type-speed.svg?react";
+export { default as TypeTechnical } from "./type-technical.svg?react";
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig({
     host: "::",
     port: 8000,
   },
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), svgr(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
