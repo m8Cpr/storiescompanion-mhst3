78497c994d391167f0b029473b16ab271401b88b
M8Cpr <matteo.caprotti8@gmail.com>
2026-03-24 17:06:20 +0100

    feat: monster detail page

--- src/pages/MonsterDetail.tsx
+++ src/pages/MonsterDetail.tsx
@@ -1,14 +1,21 @@
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
-
+import { CombatData } from "@/components/monster-details/combat-data/CombatData";
+import IncompleteData from "@/components/monster-details/IncompleteData";
+import { MonsterHeader } from "@/components/monster-details/MonsterHeader";
+import { MonstieInfo } from "@/components/monster-details/monstie-info/MonstieInfo";
 import { TRANSLATION_KEYS } from "@/i18n/keys";
-import NotFound from "@/pages/NotFound";
 import { useMonsterStore } from "@/stores/monsterStore";
+import { cn } from "@/utils/lib";
+import { ArrowLeft } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import { Link, useParams } from "react-router-dom";
+import NotFound from "./NotFound";
+
+const { COMMON } = TRANSLATION_KEYS;
 
 export default function MonsterDetail() {
   const { slug } = useParams<{ slug: string }>();
   const getBySlug = useMonsterStore((s) => s.getBySlug);
-  const { t } = useTranslation("common");
+  const { t } = useTranslation(["common", "monster"]);
 
   const monster = slug ? getBySlug(slug) : undefined;
 
@@ -16,11 +23,37 @@ export default function MonsterDetail() {
 
   return (
     <>
-      <h1>{monster.name}</h1>
+      <section className="flex flex-col gap-2 lg:gap-4">
+        <Link
+          to="/"
+          className={cn(
+            "inline-flex items-center gap-2",
+            "text-muted-foreground hover:text-foreground",
+            "transition-colors"
+          )}
+        >
+          <ArrowLeft className="size-4" />
+          {t(COMMON.BACK_TO_HOME)}
+        </Link>
 
-      <section>
-        <p>{t(TRANSLATION_KEYS.MONSTER.DETAIL)}</p>
+        {/* TODO - dynamic display */}
+        <IncompleteData />
       </section>
+
+      <MonsterHeader monster={monster} />
+
+      <CombatData combatData={monster.combatData} />
+
+      {/* TODO - Boss monster patterns - needs better scraping*/}
+
+      {monster.monstie && (
+        <MonstieInfo
+          monstie={monster.monstie}
+          ridingActions={monster.ridingActions}
+          habitatDetails={monster.habitatDetails}
+          howToGet={monster.howToGet}
+        />
+      )}
     </>
   );
 }
