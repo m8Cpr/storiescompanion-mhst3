221a7c677c9cdfdaf04864511c922139c596933e
M8Cpr <matteo.caprotti8@gmail.com>
2026-03-26 15:14:04 +0100

    chore: now displaying last commit tile as well

--- vite.config.ts
+++ vite.config.ts
@@ -8,19 +8,22 @@ import pkg from "./package.json";
 
 function getGitInfo() {
   try {
-    const branch = execSync("git rev-parse --abbrev-ref HEAD")
+    const [commitTitle, hash] = execSync('git log -1 --format="%s%n%h"')
       .toString()
-      .trim();
-    const hash = execSync("git rev-parse --short HEAD").toString().trim();
-    return { branch, hash };
+      .trim()
+      .split("\n");
+
+    const branch = execSync("git branch --show-current").toString().trim();
+
+    return { branch, hash, commitTitle };
   } catch {
-    return { branch: "unknown", hash: "000000" };
+    return { branch: "unknown", hash: "000000", commitTitle: "unknown" };
   }
 }
 
 export default defineConfig(({ mode }) => {
   const base = mode === "production" ? "/storiescompanion-mhst3/" : "/";
-  const { branch, hash } = getGitInfo();
+  const { branch, hash, commitTitle } = getGitInfo();
 
   return {
     base,
@@ -37,6 +40,7 @@ export default defineConfig(({ mode }) => {
     define: {
       __APP_VERSION__: JSON.stringify(pkg.version),
       __GIT_BRANCH__: JSON.stringify(branch),
+      __GIT_COMMIT__: JSON.stringify(commitTitle),
       __GIT_HASH__: JSON.stringify(hash),
       __BASE_PATH__: JSON.stringify(base),
     },
